6f7639ae59d6604012ed6981650250405838b2a1
Ender <woodwind.turbeville@gmail.com>
2020-10-03 19:32:10 -0500

    Added new js for Employee arguments and testing

--- Index.js
+++ Index.js
@@ -1,3 +1,4 @@
+const inquirer = require('inquirer');
 const startApp = require('./lib/Employee');
 
 new startApp().initializeApp();
---
+++ __tests__/Employee.test.js
@@ -0,0 +1,22 @@
+const Employee = require('../lib/Employee');
+
+// To test for name given from user input
+test('Can get name via getName()', () => {
+    const testValue = "Jerry";
+    const e = new Employee(testValue);
+    expect(e.getName()).toBe(testValue);
+});
+
+// To test for ID given at user input
+test('Can get id via getId()', () => {
+    const testValue = 1;
+    const e = new Employee("Jerry", testValue);
+    expect(e.getId()).toBe(testValue);
+});
+
+// To test for an Email given from user input
+test('Can get email via getEmail()', () => {
+    const testValue = "freeram@totallynotavirus.com";
+    const e = new Employee("Jerry", 1, testValue);
+    expect(e.getEmail()).toBe(testValue);
+});
